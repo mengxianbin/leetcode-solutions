37146210c2d5c6a04ff9a26e944ec23e772de7c6
MengXianBin <mengxianbindev@163.com>
2020-02-19 09:28:57 +0800

    just format

--- src/main/java/leetcode/editor/cn/P402_RemoveKDigits.java
+++ src/main/java/leetcode/editor/cn/P402_RemoveKDigits.java
@@ -53,8 +53,8 @@ public class P402_RemoveKDigits {
         public String removeKdigits(String num, int k) {
             LinkedList<Character> stack = new LinkedList<>();
 
-            for(char digit : num.toCharArray()) {
-                while(stack.size() > 0 && k > 0 && stack.peekLast() > digit) {
+            for (char digit : num.toCharArray()) {
+                while (stack.size() > 0 && k > 0 && stack.peekLast() > digit) {
                     stack.removeLast();
                     --k;
                 }
@@ -62,15 +62,15 @@ public class P402_RemoveKDigits {
             }
 
             /* remove the remaining digits from the tail. */
-            for(int i=0; i<k; ++i) {
+            for (int i = 0; i < k; ++i) {
                 stack.removeLast();
             }
 
             // build the final string, while removing the leading zeros.
             StringBuilder ret = new StringBuilder();
             boolean leadingZero = true;
-            for(char digit: stack) {
-                if(leadingZero && digit == '0') continue;
+            for (char digit : stack) {
+                if (leadingZero && digit == '0') continue;
                 leadingZero = false;
                 ret.append(digit);
             }
