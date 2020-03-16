4aa9388a6e9bc06ab742e10c687078e24366f8e0
MengXianBin <mengxianbindev@163.com>
2020-03-16 10:38:03 +0800

    feat: PInterview0106_CompressString

---
+++ src/main/java/leetcode/editor/cn/PInterview0106_CompressString/SolutionTest.java
@@ -0,0 +1,23 @@
+package leetcode.editor.cn.PInterview0106_CompressString;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+class SolutionTest {
+
+    private void test(String expected, String input) {
+        String actual = new Solution1().compressString(input);
+        Assertions.assertEquals(expected, actual);
+    }
+
+    /* "aabcccccaa" */
+    @Test
+    public void test1() {
+        test("a2b1c5a3", "aabcccccaaa");
+    }
+
+    @Test
+    public void test2() {
+        test("abbccd", "abbccd");
+    }
+}
